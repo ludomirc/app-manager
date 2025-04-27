257cc49cda296609cb05c991a9dee1d32fd8a37b
Beniamin Czaplicki <beniamin.czaplicki@gmail.com>
2025-04-27 19:32:18 +0100

    Update entrypoint.sh
    added API_URL variable update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+# Inject API_URL into frontend config.json
+echo "Injecting API URL into frontend config.json..."
+echo "{
+  \"apiUrl\": \"${API_URL}\"
+}" > /usr/share/nginx/html/assets/config.json
+
 echo "Starting Nginx (Angular frontend)..."
 service nginx start
 

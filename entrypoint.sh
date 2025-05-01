01fc9a9fbcf5254865b98fe4a86017ab8f554614
Beniamin Czaplicki <beniamin.czaplicki@gmail.com>
2025-05-01 08:48:47 +0100

    Update entrypoint.sh
    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+# Fail fast
+set -e
+
 # Inject API_URL into frontend config.json
 echo "Injecting API URL into frontend config.json..."
 echo "{
   \"apiUrl\": \"${API_URL}\"
 }" > /usr/share/nginx/html/assets/config.json
 
-echo "Starting Nginx (Angular frontend)..."
-service nginx start
-
 echo "Starting Spring Boot backend..."
-exec java -jar /app/app.jar
+java -jar /app/app.jar &
+
+echo "Starting Nginx (Angular frontend)..."
+exec nginx -g "daemon off;"
